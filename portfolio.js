0dc387d993a0dfd8c087361a3c8f10cc3b211e21
tricia-casey <tricia.casey9614@gmail.com>
2022-07-24 14:26:36 -0400

    added html and javascript to contact me page.

---
+++ portfolio.js
@@ -0,0 +1,26 @@
+//function menuOnClick() {
+    //document.getElementById("menu-bar").classList.toggle("change");
+    //document.getElementById("nav").classList.toggle("change");
+    //document.getElementById("menu-bg").classList.toggle("change-bg");
+  //}
+
+  //const Firstname = document.getElementById('FirstName')
+  //const Lastname = document.getElementById('LastName')
+  //const Email = document.getElementById('Email')
+  //const form = document.getElementById('form')
+  //const errorElement = document.getElementById('error')
+
+  //form.addEventListener(Submit, (e) => {
+   // let messages = []
+   // if (FirstName.value === '' || FirstName.value == null) {
+   //   messages.push('Name is Required')
+   // }
+
+   // if (messages.length > 0) {
+   //   e.preventDefault()
+   //   errorElement.innerText = messages.join(', ')    
+   // }
+   // e.preventDefault()
+ // })
+
+
